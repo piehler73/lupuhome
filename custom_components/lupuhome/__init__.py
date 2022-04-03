d46188ef5b7c3f6f7485ba8688d88be300686fe3
piehler73 <ahaagner@gmx.de>
2022-04-03 10:27:39 +0200

    issues fiex

--- custom_components/lupuhome/__init__.py
+++ custom_components/lupuhome/__init__.py
@@ -66,8 +66,10 @@ async def async_setup(hass: core.HomeAssistant, config: ConfigType) -> bool:
     _LOGGER.debug("CONF_USERNAME=%s", CONF_USERNAME)     
     _LOGGER.debug("CONF_PASSWORD=%s", CONF_PASSWORD) 
     _LOGGER.debug("LEN(config)=%s", len(config)) 
+    _LOGGER.debug("config=%s", str(config)) 
     for key, value in config.items():        
-        print(key, ' : ', value)
+        #print(key, ' : ', value)
+        _LOGGER.debug("key=%s, value=%s", key, value) 
 
     if (config != None):
         _LOGGER.debug("config is not null")  
